3ceec9c335f9cfd9aecf75e00bab9d1490097387
Luis Eduardo Pereira Mendes <86117347+luiseduardomendes@users.noreply.github.com>
2021-07-10 10:49:43 -0300

    Create ex003.c

---
+++ etapa 1/Manipulação de strings/ex003.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+void scan(char frase[81], int Palavras[500][40]);
+void main()
+{
+    char frase[81];
+    int Palavras[500][40];
+    scan(frase, Palavras);
+    return 0;
+}
+void scan(char frase[81], int Palavras[500][40])
+{
+    int i, j, linha = 0, coluna = 0;
+    do
+    {
+        printf("Digite uma frase: \n");
+        gets(frase);
+        strcat(frase, " \0");
+        for (i = 0; i < 81; i ++){
+            if (frase[i] == ' '){
+                Palavras[linha][coluna] = i;
+                coluna ++;
+            }
+            if (frase[i] == '\0'){
+                linha ++;
+            }
+        }
+    }
+    while (frase [0] != '0');
+    for (i = 0; i < 5; i ++){
+        for (j = 0; j < 10; j ++)
+            printf("%d ", Palavras[i][j]);
+        printf("\n");
+    }
+}
