af5db1de7f0c8400d6813101941cb6be9635720d
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2021-07-14 13:50:19 -0300

    Create ex001.c

---
+++ etapa 1/Registros/ex001.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int i;
+struct registro{
+        int vetor[3];
+        int num_int;
+    };
+struct registro g_reg;
+
+void leitura_dados();
+void imprimir_valores();
+void soma_vetor_nums();
+void imprimir_soma();
+
+int main()
+{
+    leitura_dados();
+    imprimir_valores();
+    soma_vetor_nums();
+    imprimir_soma();
+    return 0;
+}
+
+void leitura_dados()
+{
+    for (i = 0; i < 3; i ++){
+        printf("\nDigite o elemento %d do vetor: ", i + 1);
+        scanf("%d", &g_reg.vetor[i]);
+    }
+}
+
+void imprimir_valores()
+{
+    for (i = 0; i < 3; i ++)
+        printf("%d " , g_reg.vetor[i]);
+}
+
+void soma_vetor_nums()
+{
+    g_reg.num_int = 0;
+    for (i = 0; i < 3; i ++){
+        g_reg.num_int += g_reg.vetor[i];
+    }
+}
+
+void imprimir_soma()
+{
+    printf("\n\nO valor da soma dos elementos eh %d", g_reg.num_int);
+}
