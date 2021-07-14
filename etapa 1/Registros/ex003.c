2b47572baf35478d3e157ab262b73acf0e2c6a11
Luis Eduardo Pereira Mendes <86117347+luiseduardomendes@users.noreply.github.com>
2021-07-14 20:57:53 -0300

    Update ex003.c

--- etapa 1/Registros/ex003.c
+++ etapa 1/Registros/ex003.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 
 struct reg{
     float x;
     float y;
 };
 struct reg leitura();
+float distancia(struct reg ponto1, struct reg ponto2);
+bool triangulo(float d1, float d2, float d3);
 
 int main()
 {
     struct reg ponto1, ponto2, ponto3;
+    float d1, d2, d3;
 
     ponto1 = leitura();
     ponto2 = leitura();
     ponto3 = leitura();
-    distancia(ponto1, ponto2);
-    distancia(ponto2, ponto3);
-    distancia(ponto1, ponto3);
+    d1 = distancia(ponto1, ponto2);
+    d2 = distancia(ponto2, ponto3);
+    d3 = distancia(ponto1, ponto3);
+    if (triangulo(d1, d2, d3)){
+        printf("Eh possivel formar um triangulo a partir dos dados inseridos\n");
+        printf("Os lados do triangulo sao %.2f %.2f %.2f", d1, d2, d3);
+    }
+    else{
+        printf("Nao eh possivel formar um triangulo a partir dos dados inseridos\n");
+    }
     return 0;
 }
 
@@ -32,14 +43,15 @@ struct reg leitura()
 }
 float distancia(struct reg ponto1, struct reg ponto2 )
 {
-    int d = sqrt(sqr(ponto2.x - ponto1.x) + sqr(ponto2.y - ponto1.y));
+    float d;
+    d = sqrt(pow((ponto2.x - ponto1.x), 2) + pow(ponto2.y - ponto1.y, 2));
     return d;
 }
 bool triangulo(float d1, float d2, float d3)
 {
     bool resp = true;
     if ((d1 > d2 + d3) || (d2 > d1 + d3) || (d3 > d1 + d2)){
-        resp = false
+        resp = false;
     }
     return resp;
 }
