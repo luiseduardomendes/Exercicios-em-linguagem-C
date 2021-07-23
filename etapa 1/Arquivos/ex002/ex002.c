253471ae3fd1b57dffeea6dc492272599b1389fa
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2021-07-23 16:30:08 -0300

    Update ex002.c

--- etapa 1/Arquivos/ex002/ex002.c
+++ etapa 1/Arquivos/ex002/ex002.c
@@ -3,17 +3,24 @@
 #include <string.h>
 
 FILE* arquivo;
+char linha[101];
+char letra;
+int i;
+
+void primeira_palavra();
+void ultima_palavra();
 
 int main()
 {
-    char linha[101];
-    if (fopen("texto_ex051.txt", "w") != NULL){
+
+
+    arquivo = fopen("E:\\teste C\\ex051.txt", "ab+");
+    if (arquivo != NULL){
         printf("Arquivo encontrado...\n\n");
-        arquivo = fopen("texto_ex051.txt", "w");
     }
     else{
         printf("Arquivo nao encontrado, criando novo...\n\n");
-        arquivo = fopen("texto_ex051.txt", "wb+");
+        arquivo = fopen("E:\\teste C\\ex051.txt", "wb");
     }
     fputs("Digite um texto: \n", stdout);
     fgets(linha, 101, stdin);
@@ -23,6 +30,53 @@ int main()
         fgets(linha, 101, stdin);
     }
 
+    rewind(arquivo);
+
+    printf("\nprimeiras palavras das linhas pares: \n");
+
+    primeira_palavra();
+
+    printf("\nultimas palavras de cada linha: \n");
+
+    rewind(arquivo);
+
+    ultima_palavra();
+
     fclose(arquivo);
+
     return 0;
+}
+
+void primeira_palavra()
+{
+    while (!feof(arquivo)){
+        fgets(linha, 101, arquivo);
+        letra = 'a';
+        while (letra != ' '){
+            letra = fgetc(arquivo);
+            if (((letra >= 'a') && (letra >= 'z')) || ((letra >= 'A') && (letra >= 'Z')))
+            {
+                printf("%c", letra);
+            }
+            if (letra == ' '){
+                printf("\n");
+                fgets(linha, 101, arquivo);
+            }
+            if (letra == EOF){
+                break;
+            }
+        }
+    }
+}
+
+void ultima_palavra()
+{
+    while (!feof(arquivo)){
+        fgets(linha, 101, arquivo);
+        i = strlen(linha);
+        while (linha[i] != ' ')
+            i --;
+        for(int j = i + 1; j < strlen(linha); j ++)
+            printf("%c", linha[j]);
+    }
 }
